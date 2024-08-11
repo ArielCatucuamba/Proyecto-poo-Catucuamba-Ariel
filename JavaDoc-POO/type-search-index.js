6ef518945cf25a86b14d6203fa32e57e19b93e3e
ArielCatucuamba <davidcatudiaz22@gmail.com>
2024-08-11 18:46:17 -0500

    Java Doc de mi proyecto
    Este apartado cuenta con todo lo que se genero al crear un javadoc desde intelliJ IDEA , por lo general al usar el comentario reservado /**
    **/ que es para generar la documentacion

---
+++ JavaDoc-POO/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cliente"},{"p":"<Unnamed>","l":"Conexion"},{"p":"<Unnamed>","l":"DialogoPersonalizado"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"PantallaInicio"},{"p":"<Unnamed>","l":"RolAdministrador"},{"p":"<Unnamed>","l":"RolRecepcionista"}];updateSearchResults();
